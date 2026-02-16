a581bd40762e1d1d0de4d523869acf446b1dc62f
Macohi <sinconsistencia@gmail.com>
2026-02-15 21:46:11 -0600

    play miss anim on... miss

---
+++ koyalPacks/K1LL_SOMEBODY/characters/k1llsomebody_bf.lua
@@ -0,0 +1,8 @@
+function noteMiss(id, direction, noteType, isSustainNote)
+	-- Called after the note miss calculations
+	-- Player missed a note by letting it go offscreen
+
+	if boyfriendName == 'k1llsomebody_bf' then
+		callMethod('boyfriend.playAnim', {'miss'})
+	end
+end
